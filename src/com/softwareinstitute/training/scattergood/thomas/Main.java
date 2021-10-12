ddce38bb93bf29ea4a4663642664a7482ad8d164
unknown <tscattergood@softwareiinstitute.com>
2021-10-12 09:37:06 +0100

    This is my first commit of the demo hellow world project for megacohourt 1

---
+++ src/com/softwareinstitute/training/scattergood/thomas/Main.java
@@ -0,0 +1,25 @@
+package com.softwareinstitute.training.scattergood.thomas;
+
+public class Main
+{
+
+    public static void main(String[] args) //main method
+    {
+        char a = 5;
+        char b = 3;
+        char c = 3.4
+        int num1 = a+b;
+        int num2 = a*b;
+        int num3 = a-b;
+        System.out.println("Hello World!");
+        System.out.println()
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+
+        boolean b1 = 5 > 4;
+        System.out.println(b1);
+
+
+    }
+}
